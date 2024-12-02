09747b9ccb38abb0ebcc9b070ae1c9d64776f584
19thMarch <161609366+nvtqx1@users.noreply.github.com>
2024-12-02 14:59:37 +0700

    1

--- src/main/java/com/CSDL/filter/ExceptionFilter.java
+++ src/main/java/com/CSDL/filter/ExceptionFilter.java
@@ -1,4 +1,26 @@
 package com.CSDL.filter;
 
-public class ExceptionFilter {
+import com.CSDL.dto.ErrorMessage;
+import com.CSDL.utils.JsonUtils;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "ExceptionFilter", value = "/*")
+public class ExceptionFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        try {
+            chain.doFilter(request, response);
+        } catch (RuntimeException e) {
+            ErrorMessage errorMessage = new ErrorMessage(400, e.toString());
+            JsonUtils.out(response, errorMessage, HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
 }
