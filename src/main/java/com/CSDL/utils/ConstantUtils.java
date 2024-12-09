11a83ff6e6bfcf53f4f460ac583d2c0d3e13ef8b
19thMarch <161609366+nvtqx1@users.noreply.github.com>
2024-12-09 15:17:18 +0700

    Update ConstantUtils.java

--- src/main/java/com/CSDL/utils/ConstantUtils.java
+++ src/main/java/com/CSDL/utils/ConstantUtils.java
@@ -2,9 +2,9 @@ package com.CSDL.utils;
 
 public interface ConstantUtils {
     int DB_PORT = 3306;
-    String SERVER_NAME = "localhost";
-    String DB_NAME = "bookshopdb";
-    String DB_USERNAME = "root";
-    String DB_PASSWORD = "190305";
-    String IMAGE_PATH = "E:\\CSDL\\image"; // Dùng "C:\\var\\webapp\\images" trên Windows
+    String SERVER_NAME = "sql303.infinityfree.com";
+    String DB_NAME = "if0_37863281_bookshopdb";
+    String DB_USERNAME = "if0_37863281";
+    String DB_PASSWORD = "Trung19032005 ";
+    String IMAGE_PATH = ".../image"; // Dùng "C:\\var\\webapp\\images" trên Windows
 }
