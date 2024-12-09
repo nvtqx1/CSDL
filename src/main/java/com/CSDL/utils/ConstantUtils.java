c394748943d814587a2b1cc3db3469b22304db25
19thMarch <161609366+nvtqx1@users.noreply.github.com>
2024-12-09 15:41:54 +0700

    last

--- src/main/java/com/CSDL/utils/ConstantUtils.java
+++ src/main/java/com/CSDL/utils/ConstantUtils.java
@@ -5,7 +5,7 @@ public interface ConstantUtils {
     String SERVER_NAME = "sql303.infinityfree.com";
     String DB_NAME = "if0_37863281_bookshopdb";
     String DB_USERNAME = "if0_37863281";
-    String DB_PASSWORD = "Trung19032005 ";
+    String DB_PASSWORD = "Trung19032005";
     String IMAGE_PATH = "/image"; // Dùng "C:\\var\\webapp\\images" trên Windows
 
 }
