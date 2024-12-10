92f259ad722e526c3bc7c547452309028955b7db
buiductrong01012005 <160731531+buiductrong01012005@users.noreply.github.com>
2024-12-10 14:08:45 +0700

    ....

--- src/main/java/com/CSDL/utils/ConstantUtils.java
+++ src/main/java/com/CSDL/utils/ConstantUtils.java
@@ -1,10 +1,19 @@
 package com.CSDL.utils;
 
+import javax.servlet.ServletContext;
+
 public interface ConstantUtils {
-    int DB_PORT = 22139;
-    String SERVER_NAME = "mysql-3b8f0f5f-bull.i.aivencloud.com";
-    String DB_NAME = "bookshopdb";
-    String DB_USERNAME = "avnadmin";
-    String DB_PASSWORD = "AVNS_7QkJvtymr6rvTwhhvg8";
-    String IMAGE_PATH = "F:\\CSDL\\var\\webapp\\images";
+//    int DB_PORT = 3306;
+//    String SERVER_NAME = "sql303.infinityfree.com";
+//    String DB_NAME = "if0_37863281_bookshopdb";
+//    String DB_USERNAME = "if0_37863281";
+//    String DB_PASSWORD = "Trung19032005";
+//    String IMAGE_PATH = ".../image"; // Dùng "C:\\var\\webapp\\images" trên Windows
+        int DB_PORT = 22139;
+        String SERVER_NAME = "mysql-3b8f0f5f-bull.i.aivencloud.com";
+        String DB_NAME = "bookshopdb";
+        String DB_USERNAME = "avnadmin";
+        String DB_PASSWORD = "AVNS_7QkJvtymr6rvTwhhvg8";
+        String IMAGE_PATH = String.valueOf(System.getProperty("user.dir")).substring(0,System.getProperty("user.dir").length()-3)+"webapps\\ROOT\\image";
 }
+
